46eb5793444978918897fae7b7c0495735e7b109
nikitacreation35-dot <nikitacreation35@gmail.com>
2026-07-21 11:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,11 @@ Random Forest Machine Learning Model
 """,
 )
 
-demo.launch()
+import os
+
+port = int(os.environ.get("PORT", 7860))
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=port
+)
